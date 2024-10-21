03d255f7a1ccde85bfbe05288c720e48dc864c5b
Ali anjo <alianjo178@gmail.com>
2024-10-21 18:38:47 +0330

    Add: Function

--- functions/main.go
+++ functions/main.go
@@ -7,6 +7,20 @@ func ObjectTaker(i ...any) any {
 }
 
 func main() {
-	i := ObjectTaker([]int{1, 2, 3, 4}, map[int]string{1: "ali", 2: "sadeq"}, 3)
-	fmt.Print(i.(int))
+	i := ObjectTaker([]int{1, 2, 3, 4}, map[int]string{1: "John", 2: "Frank"}, 3)
+	fmt.Print(i)
+
+	nextInt := func() func() int {
+		i := 0
+		return func() int {
+			i++
+			return i
+		}
+	}
+	nextIntFunc := nextInt()
+	fmt.Println(nextIntFunc())
+	fmt.Println(nextIntFunc())
+	fmt.Println(nextIntFunc())
+	fmt.Println(nextIntFunc())
+
 }
