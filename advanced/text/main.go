0acf67d2a1e7f095c222a4b4765786f0b53d7620
Ali Anjo <alianjo178@gmail.com>
2024-10-31 16:40:33 +0330

    Add: Regex example

---
+++ advanced/text/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"regexp"
+)
+
+var P = fmt.Println
+
+func main() {
+	r, err := regexp.Compile("\\d{3}-\\d{3}-\\d{4}")
+	if err != nil {
+		P(err)
+		return
+	}
+	example_string := "Phone number 123-456-7489"
+	P(r.FindString(example_string))
+	P(r.FindStringIndex(example_string))
+	P(r.FindStringSubmatchIndex(example_string))
+}
