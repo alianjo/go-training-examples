7f70a87d4aa0dc34308cbf37c6f9fa79b3052df4
Ali Anjo <alianjo178@gmail.com>
2024-11-01 10:51:54 +0330

    Add time with fmt

--- intermediate/time/main.go
+++ intermediate/time/main.go
@@ -21,4 +21,7 @@ func main() {
 	P(diff.Hours())
 	P(diff.Microseconds())
 
+	t := time.Now()
+	fmt.Printf("%d-%02d-%02dT-%02d:%02d:%02d", t.Year(), t.Month(), t.Day(), t.Hour(), t.Minute(), t.Second())
+
 }
